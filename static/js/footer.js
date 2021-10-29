4a5c4e8876244fe7a15b7a9542b1b1aec27ff760
princebangladesh <judgementprince222@gmail.com>
2021-10-29 06:44:29 +0600

    Footer Update

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
-
+  function FooterSlide() {
+    if($(window).width() < 991 ){
       $(".footerHover div").click(function(e) {
         e.preventDefault();
         
         $("i",this).toggleClass("fa-plus fa-minus")
-        $(".footerHover div").not(this).find("span i").removeClass("fa-minus").addClass("fa-plus")
+        $(".footerHover div").not(this).find(".iconplus i").removeClass("fa-minus").addClass("fa-plus")
 
         var $div = $(this).next('.footerContent');
         $(".footerContent").not($div).slideUp(800)
-        
-       
-
           if ($div.is(":visible")) {
               $div.slideUp(500)
-              
           }  
           else {
              $div.slideDown(800)
-             
-
-             
-
-             
           }
       });
+    }
+    else{
+      $(".footerHover div").slideDown()
+    }
+    
+  }
+  $(window).resize(function () {
+    FooterSlide()
+  })
+  FooterSlide()
   
-      
-       
-  });
+});
