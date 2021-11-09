c1fae86221855b9b3324a026b96ebb6219e166d5
princebangladesh <judgementprince222@gmail.com>
2021-11-09 18:49:42 +0600

    Footer Updatye

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   function FooterSlide() {
     if($(window).width() < 991 ){
-      $(".footerHover div").click(function(e) {
+      $(".footerHover .borderTB").click(function(e) {
         e.preventDefault();
         
         $("i",this).toggleClass("fa-plus fa-minus")
-        $(".footerHover div").not(this).find(".iconplus i").removeClass("fa-minus").addClass("fa-plus")
+        $(".footerHover .borderTB").not(this).find(".iconplus i").removeClass("fa-minus").addClass("fa-plus")
 
         var $div = $(this).next('.footerContent');
         $(".footerContent").not($div).slideUp(800)
@@ -23,8 +23,29 @@ $(document).ready(function(){
     
   }
   $(window).resize(function () {
-    FooterSlide()
+    if($(window).width() < 991 ){
+      $(".footerHover .borderTB").click(function(e) {
+        e.preventDefault();
+        
+        $(".footerHover .borderTB").not(this).find(".iconplus i").removeClass("fa-minus").addClass("fa-plus")
+
+        var $div = $(this).next('.footerContent');
+        $(".footerContent").not($div).slideUp(800)
+          if ($div.is.not(":visible")) {
+              $div.slideDown(800)
+              // $("i",this).toggleClass("fa-plus fa-minus")
+          }
+          else{
+            $div.slideUp(500)
+
+          }
+
+      });
+    }
+    else{
+      $(".footerHover div").slideDown()
+    }
   })
-  FooterSlide()
+      FooterSlide()
   
 });
